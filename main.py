69d3aabc21d738bc341c3f8c023d18741d899622
LeGDieS <legdies@gmail.com>
2024-03-25 00:40:55 +0000

    Improved functionality of findpost command by using list of images instead only one.

--- main.py
+++ main.py
@@ -14,22 +14,43 @@ from discord.ui import Button
 api = TR34Api()
 bot = discord.Bot()
 
+
+
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
 
+from discord.ext import pages
 @bot.slash_command()
-async def findpost(ctx, arg):
-    post = await api.get_post(arg)
-    embed = discord.Embed(title=f"Post {arg}")
-    pages = [
+async def findpost(ctx, tags):
+    posts = await api.search_posts(tags=[tags], limit=5, page=0)
 
-        Page(embeds=[embed.set_image(url=f"{post.main.file}"),
-                     embed.add_field(name="tags: ", value=f"```{' '.join(tuple(post.tags))}```")])
+    if not posts:
+        await ctx.respond("Постов с такими тегами не найдено.", ephemeral=True)
+        return
 
-    ]
-    pagginator = Paginator(pages=pages)
-    await pagginator.respond(ctx.interaction)
+    pages = []
+
+    # Create 5 pages
+    for i in range(5):
+        page = Page(embeds=[])
+        pages.append(page)
+
+    # Add posts to pages
+    for i, post in enumerate(posts):
+        embed = discord.Embed(title=f"Page {i + 1}", description=f"If post cant be loaded\n {post.main.file}", color=discord.Color.random())  # Обновляем embed для каждого поста
+        embed.add_field(name="tags: ",value=f"{' '.join(tuple(post.tags))}")
+        embed.set_image(url=f"{post.main.file}")
+        pages[i].embeds.append(embed)
+
+    # Handle empty pages
+    if len(posts) < 5:
+        for i in range(len(posts), 5):
+            pages[i].embeds.append(discord.Embed(title=f"Page {i + 1}", description="No posts found."))
+
+    paginator = Paginator(pages=pages)
+    await paginator.respond(ctx.interaction)
+
 
 
 
